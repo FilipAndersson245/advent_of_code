68badc2c6b1c34f4d7191fb6fad2f1b2724a830d
FilipAndersson245 <17986183+FilipAndersson245@users.noreply.github.com>
2023-12-07 22:23:00 +0100

    Updated util lib

--- aoc-util/src/lib.rs
+++ aoc-util/src/lib.rs
@@ -9,13 +9,11 @@ pub use point::Point;
 #[cfg(feature = "parsing")]
 pub use parsing;
 
-#[cfg(feature = "gxhash")]
-pub use gxhash::{GxHashMap as HashMap, GxHashSet as HashSet};
+pub type HashMap<K, V> = std::collections::HashMap<K, V, gxhash::GxHasher>;
+pub type HashSet<T> = std::collections::HashSet<T, gxhash::GxHasher>;
 
-#[cfg(not(feature = "gxhash"))]
-pub use std::collections::{HashMap, HashSet};
-
-// pub type IndexMap<T, V> = indexmap::IndexMap<T, V, gxhash::GxHasher>;
+pub type IndexMap<T, V> = indexmap::IndexMap<T, V, gxhash::GxHasher>;
+pub type IndexSet<T> = indexmap::IndexSet<T, gxhash::GxHasher>;
 
 pub mod prelude {
     pub use crate::{Direction, Grid, HashMap, HashSet, Point};
